7f2ae5823652acfe04a8af3c788cff57f01a0925
wushuai <659-4gpe145ln@dingtalk.com>
2023-12-13 17:38:57 +0800

    P137:explain语句输出的各个列详述

---
+++ classnote_high/explain相关数据准备.sql
@@ -0,0 +1,103 @@
+1. 建表
+CREATE TABLE s1 (
+                    id INT AUTO_INCREMENT,
+                    key1 VARCHAR(100),
+                    key2 INT,
+                    key3 VARCHAR(100),
+                    key_part1 VARCHAR(100),
+                    key_part2 VARCHAR(100),
+                    key_part3 VARCHAR(100),
+                    common_field VARCHAR(100),
+                    PRIMARY KEY (id),
+                    INDEX idx_key1 (key1),
+                    UNIQUE INDEX idx_key2 (key2),
+                    INDEX idx_key3 (key3),
+                    INDEX idx_key_part(key_part1, key_part2, key_part3)
+) ENGINE=INNODB CHARSET=utf8;
+
+CREATE TABLE s2 (
+                    id INT AUTO_INCREMENT,
+                    key1 VARCHAR(100),
+                    key2 INT,
+                    key3 VARCHAR(100),
+                    key_part1 VARCHAR(100),
+                    key_part2 VARCHAR(100),
+                    key_part3 VARCHAR(100),
+                    common_field VARCHAR(100),
+                    PRIMARY KEY (id),
+                    INDEX idx_key1 (key1),
+                    UNIQUE INDEX idx_key2 (key2),
+                    INDEX idx_key3 (key3),
+                    INDEX idx_key_part(key_part1, key_part2, key_part3)
+) ENGINE=INNODB CHARSET=utf8;
+
+2.设置参数 log_bin_trust_function_creators
+set global log_bin_trust_function_creators=1;
+
+3.创建函数
+DELIMITER
+CREATE FUNCTION rand_string1(n INT)
+    RETURNS VARCHAR(255) #该函数会返回一个字符串
+BEGIN
+DECLARE chars_str VARCHAR(100) DEFAULT
+'abcdefghijklmnopqrstuvwxyzABCDEFJHIJKLMNOPQRSTUVWXYZ';
+DECLARE return_str VARCHAR(255) DEFAULT '';
+DECLARE i INT DEFAULT 0;
+WHILE i < n DO
+SET return_str =CONCAT(return_str,SUBSTRING(chars_str,FLOOR(1+RAND()*52),1));
+SET i = i + 1;
+END WHILE;
+RETURN return_str;
+END //
+DELIMITER ;
+
+4.创建存储过程
+  4.1创建往s1表中插入数据的存储过程
+    DELIMITER //
+CREATE PROCEDURE insert_s1 (IN min_num INT (10),IN max_num INT (10))
+BEGIN
+DECLARE i INT DEFAULT 0;
+SET autocommit = 0;
+REPEAT
+SET i = i + 1;
+INSERT INTO s1 VALUES(
+                         (min_num + i),
+                         rand_string1(6),
+                         (min_num + 30 * i + 5),
+                         rand_string1(6),
+                         rand_string1(10),
+                         rand_string1(5),
+                         rand_string1(10),
+                         rand_string1(10));
+UNTIL i = max_num
+END REPEAT;
+COMMIT;
+END //
+DELIMITER ;
+
+  4.2创建往s2表中插入数据的存储过程：
+  DELIMITER //
+CREATE PROCEDURE insert_s2 (IN min_num INT (10),IN max_num INT (10))
+BEGIN
+DECLARE i INT DEFAULT 0;
+SET autocommit = 0;
+REPEAT
+SET i = i + 1;
+INSERT INTO s2 VALUES(
+                         (min_num + i),
+                         rand_string1(6),
+                         (min_num + 30 * i + 5),
+                         rand_string1(6),
+                         rand_string1(10),
+                         rand_string1(5),
+                         rand_string1(10),
+                         rand_string1(10));
+UNTIL i = max_num
+END REPEAT;
+COMMIT;
+END //
+DELIMITER ;
+
+5.数据添加
+    CALL insert_s1(10001,10000);
+    CALL insert_s2(10001,10000);
